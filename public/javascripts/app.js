9d4cff1bb222ea4edab296a38992ec7961f691d4
Nathan Reginato <nathan.reginato@gmail.com>
2016-04-08 10:36:57 -0600

    Got the form all hooked up

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
-  
+var authorsArr = []
+
+  $('#add-author').on('click', function() {
+    var selected = $('#available-authors :selected').text()
+    var selectedId = $('#available-authors :selected').attr('value');
+    var el = document.getElementById('authors'),
+    elChild = document.createElement('option');
+    elChild.innerHTML = selected
+    elChild.id = selectedId
+    el.appendChild(elChild);
+
+    authorData = document.getElementById('author-data')
+    authorsArr.push(selectedId)
+    authorData.value = authorsArr
+  })
+
+  $('#delete-author').on('click', function() {
+    $('#authors').empty()
+    authorsArr = []
+  })
 
 })
