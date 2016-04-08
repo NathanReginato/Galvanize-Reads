da2c1aae69cb5640fb8aaf00f3719fd7e1c67b7a
Nathan Reginato <nathan.reginato@gmail.com>
2016-04-08 15:50:49 -0600

    Still working on edit

--- routes/books.js
+++ routes/books.js
@@ -18,7 +18,7 @@ knex('books_and_authors')
       // for (var i = 0; i < bookjoin.first_name.length; i++) {
       //   fullname.push(bookjoin.first_name[i] + ' ' + bookjoin.last_name[i])
       // }
-      res.render('view_books', {book: bookjoin });
+      res.render('view_books', { book: bookjoin });
     })
 })
 
@@ -111,18 +111,54 @@ router.get('/edit/:id', function(req, res, next) {
                                                pDes: bookjoin.description,
                                                pUrl: bookjoin.cover_url,
                                                name: authorNames,
-                                               authors: fullname });
+                                               authors: fullname,
+                                               bookId: req.params.id});
       })
     })
   })
 });
 
 router.post('/edit/editpost', function(req, res, next) {
-    console.log(req.body);
-    
-    res.redirect('/');
-});
 
+  var splitAuthorsString = req.body['authors-data'].split(',')
+
+  console.log(splitAuthorsString)
+  //Insert books with author
+  if (splitAuthorsString[0] !== '') {
+    var splitAuthors = splitAuthorsString.map(function(elem) {
+      return parseInt(elem)
+    })
+    knex('books')
+    .returning('book_id')
+    .insert({title: req.body['book-title'],
+             description: req.body.description,
+             cover_url: req.body['img-url']})
+    .then(function(id){
+      splitAuthors.forEach(function(authorsInArray){
+        knex('books_and_authors')
+        .returning('id')
+        .insert({book_id: id[0], author_id: authorsInArray})
+        .then(function(id2){
+        })
+      })
+    })
+  }
+
+  //Insert book without author(s)!
+  else {
+    console.log('update book');
+    console.log(req.body['book-id']);
+    knex('books')
+    .where('book_id', '=', parseInt(req.body['book-id']))
+    .update(
+      {title: req.body['book-title'],
+       description: req.body.description,
+       cover_url: req.body['img-url']})
+    .then(function(id){
+    })
+  }
+  res.redirect('/');
+});
 
 router.get('/delete', function(req, res, next) {
     res.render('delete_book', {
@@ -136,7 +172,8 @@ router.get('/:id', function(req, res, next) {
       .join('books', 'books_and_authors.book_id', '=', 'books.book_id')
       .join('authors', 'books_and_authors.author_id', '=', 'authors.author_id')
       .select('title', 'description', 'cover_url')
-      .select(knex.raw('array_agg(first_name) AS first_name ,array_agg(last_name) AS last_name'))
+      .select(
+        knex.raw('array_agg(first_name) AS first_name ,array_agg(last_name) AS last_name'))
       .groupBy('title', 'description', 'cover_url')
       .where({'books.book_id': req.params.id})
       .then(function(bookjoin) {
