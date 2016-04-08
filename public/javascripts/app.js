129af3c965ee61cfec49db587934b2c6a23682ad
Nathan Reginato <nathan.reginato@gmail.com>
2016-04-08 14:51:44 -0600

    Almost have edit return working

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
 var authorsArr = []
 
+var getAuthors = document.getElementById('authors');
+var elementChildren = getAuthors.children;
+for (var i = 0; i < elementChildren.length; i++) {
+    authorsArr.push(elementChildren[i].id);
+    console.log(elementChildren[i].id);
+}
+
+var authorData = document.getElementById('author-data')
+authorData.value = authorsArr
+
+
+
   $('#add-author').on('click', function() {
     var selected = $('#available-authors :selected').text()
     var selectedId = $('#available-authors :selected').attr('value');
-    var el = document.getElementById('authors'),
+
     elChild = document.createElement('option');
     elChild.innerHTML = selected
     elChild.id = selectedId
     el.appendChild(elChild);
 
-    authorData = document.getElementById('author-data')
     authorsArr.push(selectedId)
     authorData.value = authorsArr
   })
@@ -18,6 +29,7 @@ var authorsArr = []
   $('#delete-author').on('click', function() {
     $('#authors').empty()
     authorsArr = []
+    authorData.value = authorsArr
+    console.log(authorsArr);
   })
-
 })
