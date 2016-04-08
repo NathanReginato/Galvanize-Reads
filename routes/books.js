a84804e6abd27008c4eab0441468317c45044501
Nathan Reginato <nathan.reginato@gmail.com>
2016-04-07 23:12:47 -0600

    Figured out aggregation and raw queries in knex

--- routes/books.js
+++ routes/books.js
@@ -4,56 +4,54 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //req all from database
-  var books = []
-  knex('books').pluck('book_id').then(function(bookid){
-    bookid.forEach(function(elem){
-      knex('books').where({book_id: elem}).first()
-      .then(function(book){
-       return knex('books_and_authors')
-      .where({book_id: elem})
-      .pluck('author_id')
-      .then(function(id){
-        return knex('authors')
-        .whereIn('author_id', id).select('first_name', 'last_name').then(function(name){
-          
-          console.log(name);
-          console.log(book);
-        })
-      })
-
-      })
+//req all from database
+knex('books_and_authors')
+    .join('books', 'books_and_authors.book_id', '=', 'books.book_id')
+    .join('authors', 'books_and_authors.author_id', '=', 'authors.author_id')
+    .select('title')
+    .select(knex.raw('string_agg(first_name, \', \')'))
+    .groupBy('title')
+    .then(function(bookjoin) {
+      console.log(bookjoin);
     })
-  })
-  res.render('new_edit_books', { book: 'book' });
-});
+    // console.log(bookjoin);
+    // res.render('view_books', {book: bookjoin });
+})
 
 router.get('/new', function(req, res, next) {
-  //Insert book
-  res.render('new_edit_books', { title: 'New' });
+    //Insert book
+    res.render('new_edit_books', {
+        title: 'New'
+    });
 });
 
 router.post('/new', function(req, res, next) {
-  //Insert book
-  res.redirect('/');
+    //Insert book
+    res.redirect('/');
 });
 
 router.get('/edit', function(req, res, next) {
-  res.render('new_edit_books', { title: 'Edit' });
+    res.render('new_edit_books', {
+        title: 'Edit'
+    });
 });
 
 router.post('/edit', function(req, res, next) {
-  res.redirect('/');
+    res.redirect('/');
 });
 
 
 router.get('/delete', function(req, res, next) {
-  res.render('delete_book', { title: 'Delete' });
+    res.render('delete_book', {
+        title: 'Delete'
+    });
 });
 
 router.get('/:id', function(req, res, next) {
-  //req id from database
-  res.render('view_books', { title: 'item data' });
+    //req id from database
+    res.render('view_books', {
+        title: 'item data'
+    });
 });
 
 module.exports = router;
